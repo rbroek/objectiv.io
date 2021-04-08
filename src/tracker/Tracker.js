1b1b1f29bd09ce2404bfdd6d10e0e1c01d56ebed
Ivar Pruijn <ivar@pruijn.com>
2021-04-08 10:21:31 +0200

    Fix build error caused by SSR

--- src/tracker/Tracker.js
+++
@@ -1,9 +0,0 @@
-import Media from './Tracker/Media';
-import Section from './Tracker/Section';
-
-const Tracker = {
-  Media,
-  Section,
-};
-
-export default Tracker;
--- src/tracker/useInstance.js
+++
@@ -1,6 +0,0 @@
-import { useRef } from 'react';
-
-export function useInstance(value) {
-  const ref = useRef(value);
-  return ref.current;
-}
