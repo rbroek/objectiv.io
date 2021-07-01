7e396456abdf98fcfaf87258bfcbf5f6138d9437
Ivar Pruijn <ivar@objectiv.ai>
2021-07-01 16:27:09 +0200

    Fix mobile styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,10 +15,10 @@ export default function Home() {
       title={siteConfig.title}
       description={siteConfig.tagline}> {/*Description will go into a meta tag in <head />*/}
       <div className={clsx(styles.announcement)}>
-        <img src="/img/icons/icon-star.svg" />
+        <span className={styles.announcementStar}><img src="/img/icons/icon-star.svg" /></span>
           <strong>We’re Hiring!</strong> Are you mad about data science &amp; engineering? <a href="/jobs">Check out the 
           vacancy</a>!
-        <img src="/img/icons/icon-star.svg" />
+        <span className={styles.announcementStar}><img src="/img/icons/icon-star.svg" /></span>
       </div>
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className={clsx('container', styles.heroContainer)}>
@@ -27,13 +27,15 @@ export default function Home() {
             src={useBaseUrl("img/header-image.svg")}
             alt="Product Analytics Pipeline" />
           <h1 className={clsx(styles.heroTitle)}>
-            The Open Source Analytics Pipeline<br/>
+            The Open Source Analytics Pipeline
             Built For The Data Scientist
           </h1>
-          <p className={clsx(styles.heroSubTitle)}>
-            Build &amp; orchestrate reusable, stackable and interchangeable models <br />
-            that embrace a standardized event taxonomy. Straight from your Jupyter notebook.
-          </p>
+          <div className={clsx(styles.heroSubTitleWrapper)}>
+            <p className={clsx(styles.heroSubTitle)}>
+              Build &amp; orchestrate reusable, stackable and interchangeable models that embrace a 
+              standardized event taxonomy. Straight from your Jupyter notebook.
+            </p>
+          </div>
           <div className={styles.heroKeepMePosted}>
             <p>In a repository near you after summer. Want to get notified?</p>
             <KeepMePosted />
