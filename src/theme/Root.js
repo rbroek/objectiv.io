7141af026144489cbd3a0846a13e58f8be4e4193
sdirosa <surai@objectiv.ai>
2021-07-13 15:21:19 +0200

    Add tracking of app loaded

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -1,11 +1,17 @@
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import { ReactTracker, TrackerContextProvider } from "@objectiv/tracker-react";
+import {
+  ReactTracker,
+  TrackerContextProvider,
+  useTrackApplicationLoaded
+} from "@objectiv/tracker-react";
 import React from 'react';
 
 function Root({children}) {
   const { siteConfig = {} } = useDocusaurusContext();
   const { trackerEndPoint } = siteConfig.customFields;
   const tracker = new ReactTracker({ endpoint: trackerEndPoint });
+
+  useTrackApplicationLoaded(tracker);
 
   return (
     <TrackerContextProvider tracker={tracker}>
