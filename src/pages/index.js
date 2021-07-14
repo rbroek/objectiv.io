add229a758101a1ce600db95c4fa1f66c4965a5f
Ivar Pruijn <ivar@objectiv.ai>
2021-07-14 11:55:51 +0200

    Move announcement bar to its own component

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Layout from '@theme/Layout';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import KeepMePosted from '../components/keep-me-posted';
+import AnnouncementBar from '../components/announcement-bar'
 import styles from './styles.module.css';
 import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
 
@@ -14,11 +15,18 @@ export default function Home() {
     <Layout
       title={siteConfig.title}
       description={siteConfig.tagline}> {/*Description will go into a meta tag in <head />*/}
-      <div className={clsx(styles.announcement)}>
+      <AnnouncementBar 
+        title="We're Hiring!"
+        content="Join our mission crew as a Data Infra Scientist / Data Workflow Engineer."
+        ctaLink='/jobs'
+        ctaText='Check the vacancy'
+      >
+      </AnnouncementBar>
+      {/* <div className={clsx(styles.announcement)}>
         <span className={styles.announcementStar}><img src="/img/icons/icon-star.svg" /></span>
           <strong>We’re Hiring! </strong>Join our mission crew as a Data Infra Scientist / Data Workflow Engineer. <a href="/jobs">Check the vacancy</a>
         <span className={styles.announcementStar}><img src="/img/icons/icon-star.svg" /></span>
-      </div>
+      </div> */}
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className={clsx('container', styles.heroContainer)}>
           <img 
@@ -26,12 +34,12 @@ export default function Home() {
             src={useBaseUrl("img/header-image.svg")}
             alt="Product Analytics Pipeline" />
           <h1 className={clsx(styles.heroTitle)}>
-          We're on a mission to create the ultimate iterative workflow for data scientists
+            We're on a mission to create the ultimate iterative workflow for data scientists
           </h1>
           <div className={clsx(styles.heroSubTitleWrapper)}>
             <p className={clsx(styles.heroSubTitle)}>
-            Build &amp; orchestrate reusable, stackable and interchangeable SQL models that embrace a 
-standardized event taxonomy. Straight from your Notebook.
+              Build &amp; orchestrate reusable, stackable and interchangeable SQL models that embrace a 
+              standardized event taxonomy. Straight from your Notebook.
             </p>
           </div>
           <div className={styles.heroKeepMePosted}>
