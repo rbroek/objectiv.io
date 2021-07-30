12a36f8eb106068c063a04fdeeabdcc1f296ab88
Ivar Pruijn <ivar@objectiv.ai>
2021-07-30 15:59:34 +0200

    Add section tracking to Announcement Bar

--- src/components/announcement-bar/index.js
+++ src/components/announcement-bar/index.js
@@ -3,12 +3,24 @@ import Link from '@docusaurus/Link';
 import clsx from 'clsx';
 import styles from './styles.module.css';
 import {
+  useTracker,
+  ReactTracker,
+  makeSectionContext,
   useTrackLinkClick,
   makeLinkContext,
 } from '@objectiv/tracker-react';
 
 function AnnouncementBar({children, title, content, ctaLink, ctaText}) {
-  const trackCtaClick = useTrackLinkClick(makeLinkContext({ id: ctaLink, href: '/jobs', text: 'Check the vacancy' }));
+  const tracker = useTracker();
+  const AnnouncementBarTracker = new ReactTracker(tracker, {
+    location_stack: [makeSectionContext({ id: 'announcement-bar' })],
+  });
+  
+  const trackCtaClick = useTrackLinkClick(makeLinkContext({ 
+    id: ctaLink, 
+    href: '/jobs', 
+    text: 'Check the vacancy' 
+  }), AnnouncementBarTracker);
   return (
     <div className={clsx(styles.announcement)}>
       <span className={styles.announcementStar}><img src="/img/icons/icon-star.svg" /></span>
