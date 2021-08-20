cad022981190cb4be730e2e333fde9f28f4b5460
Ivar Pruijn <ivar@objectiv.ai>
2021-08-20 10:27:25 +0200

    Add cookiebot & cookie policy & privacy policy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -161,6 +161,12 @@ module.exports = {
   plugins: [path.resolve(__dirname, 'src/plugins/favicons/')],
   scripts: [
     'https://cdn.jsdelivr.net/gh/mcstudios/glightbox/dist/js/glightbox.min.js',
+    {
+      src: 'https://consent.cookiebot.com/uc.js?cbid=7498452c-872b-431a-9859-21045f83f0a0',
+      'data-cbid': '7498452c-872b-431a-9859-21045f83f0a0',
+      'data-blockingmode': 'auto',
+      id: 'Cookiebot'
+    },
   ],
   stylesheets: [
     'https://cdn.jsdelivr.net/npm/glightbox/dist/css/glightbox.min.css',
