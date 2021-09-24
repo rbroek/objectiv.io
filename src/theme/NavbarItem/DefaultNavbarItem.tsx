832122cf24ef39954c5854f3bfb5ae85edad674b
Ivar Pruijn <ivar@objectiv.ai>
2021-09-24 12:28:59 +0200

    Simplify Nav link tracking

--- src/theme/NavbarItem/DefaultNavbarItem.tsx
+++ src/theme/NavbarItem/DefaultNavbarItem.tsx
@@ -7,27 +7,32 @@
 
 import React, {useState, useRef, useEffect} from 'react';
 import OriginalDefaultNavbarItem from '@theme-original/NavbarItem/DefaultNavbarItem';
+import OriginalNavLink from '@theme-original/NavbarItem/DefaultNavbarItem';
+import type { NavLinkProps } from '@theme-original/NavbarItem/DefaultNavbarItem';
 import { trackLink } from "@objectiv/tracker-browser";
 
 export function DefaultNavbarItem({mobile = false, ...props}: Props): JSX.Element {
-  // const mobileNavbarTracker = new ReactTracker(tracker, {
-  //   location_stack: [makeOverlayContext({ id: 'hamburger-menu' })],
-  // });
-  // const desktopNavbarTracker = new ReactTracker(tracker, {
-  //   location_stack: [makeSectionContext({ id: 'desktop-menu' })],
-  // });
-
-  if(mobile) {
-    return <OriginalDefaultNavbarItem 
-      {...props} 
-      {...trackLink({ id: props.label, text: props.label, href: props.href ? props.href : props.to })}
-    />
-  }
-
   return (
     <OriginalDefaultNavbarItem 
       {...props} 
-      {...trackLink({ id: props.label, text: props.label, href: props.href ? props.href : props.to })}
+    />
+  )
+}
+
+export function NavLink({
+  activeBasePath,
+  activeBaseRegex,
+  to,
+  href,
+  label,
+  activeClassName = '',
+  prependBaseUrlToHref,
+  ...props
+}: NavLinkProps): JSX.Element {
+  return (
+    <OriginalNavLink 
+      {...props} 
+      {...trackLink({ id: label, text: label, href: href ? href : to })}
     />
   )
 }
