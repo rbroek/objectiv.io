356c32a4bd578080cc9672bbc864e0de45cf6042
Ivar Pruijn <ivar@objectiv.ai>
2021-11-09 11:02:50 +0100

    Use correct baseUrl for builds (and print it)

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   titleDelimiter: '|',
   tagline: 'Objectiv is a data collection & modeling library that puts the data scientist first.',
   url: 'https://objectiv.io/',
-  baseUrl: (environment==='production') ? '/docs/' : '/',
+  baseUrl: (environment==='prod') ? '/docs/' : '/',
   favicon: 'img/favicon/favicon.ico',
   organizationName: 'objectiv', // Usually your GitHub org/user name.
   projectName: 'objectiv.io', // Usually your repo name.
@@ -122,3 +122,4 @@ const config = {
 module.exports = config;
 
 console.log("USING OBJECTIV TRACKER ENDPOINT:", config.customFields.trackerEndPoint);
+console.log("USING BASEURL:", config.baseUrl);
