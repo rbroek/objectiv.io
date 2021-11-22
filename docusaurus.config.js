e6b1caea1795720449d30c409cc7c3a647971bc7
Ivar Pruijn <ivar@objectiv.ai>
2021-11-22 10:03:52 +0100

    Disable blog for now, so it doesn't get crawled

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -45,18 +45,7 @@ const config = {
           customCss: require.resolve('./src/css/custom.css'),
         },
         docs: false,
-        blog: {
-          blogTitle: 'Objectiv Blog',
-          blogDescription: 'Objectiv Blog',
-          blogSidebarCount: 0,
-          postsPerPage: 5,
-          showReadingTime: false,
-          editUrl: 'https://github.com/facebook/objectiv/objectiv.io/edit/master/blog/',
-        },
-        sitemap: {
-          changefreq: 'weekly',
-          priority: 0.5,
-        },
+        blog: false
       }),
     ],
   ],
